911b3de94dac235318a820f20ba954bbfc91046f
Slav Korolev <slav.korolev@nasa.gov>
2020-03-26 15:52:53 -0400

    DAS-564. Fixed problem with lat/lon in NetCDF4Merger.py.

---
+++ test/test_nominal_reproject_africa.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import unittest
+from unittest.mock import patch
+
+from reproject import HarmonyAdapter
+from harmony import BaseHarmonyAdapter
+
+from test_utils import contains, matches, TestBase
+
+
+class TestNominalReproject(TestBase):
+
+    # TEST CASE: Nominal reprojection on a single band file
+    #
+    @patch.object(BaseHarmonyAdapter, 'completed_with_local_file')
+    @patch.object(BaseHarmonyAdapter, 'cleanup')
+    def test_single_band_input(self, cleanup, completed_with_local_file):
+        """Nominal (successful) reprojection"""
+        test_data = {'granules': [
+            {'local_filename': '/home/test/data/africa.nc'}],
+            # 'format': {'interpolation': 'near',
+            #            'crs': '+proj=longlat',
+            #            'scaleExtent': {'x': [-20, 60], 'y': [0, 40]}
+            #            }
+            'format': {
+                'crs': 'CRS:84',
+                'interpolation': 'bilinear',
+                'width': 1000, 'height': 500,
+                'scaleExtent': {
+                    'x': {'min': -20, 'max': 60},
+                    'y': {'min': 10, 'max': 35}
+                },
+                # 'scaleSize': {'x': 1, 'y': 1}
+            }
+        }
+        reprojector = HarmonyAdapter(test_data)
+        reprojector.invoke()
+
+        completed_with_local_file.assert_called_once_with(contains('africa_repr.nc'), 'africa.nc',
+                                                          'application/x-netcdf')
+        cleanup.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
