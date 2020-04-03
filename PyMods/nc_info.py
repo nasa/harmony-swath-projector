c5bd82280e32a50bf1aa7a30ec439c9c49da6f72
Slav Korolev <slav.korolev@nasa.gov>
2020-04-03 14:01:01 -0400

    DAS-570. Minor corrections.

--- PyMods/nc_info.py
+++ PyMods/nc_info.py
@@ -1,11 +1,7 @@
-import logging
-import json
-import rasterio
-import re
 import argparse
-from PyMods.reproject import to_object
-from netCDF4 import Dataset, ma
-import numpy as np
+import logging
+from netCDF4 import Dataset
+
 
 # walk the group tree.
 def walktree(top):
@@ -15,6 +11,7 @@ def walktree(top):
         for children in walktree(value):
             yield children
 
+
 class NC4Info:
     def __init__(self, ncfile):
         self.rootgroup = Dataset(ncfile)
@@ -22,13 +19,13 @@ class NC4Info:
         self.vars_meta = set()
         self.dims = set()
         self.coords = set()
-        self.auxs = set() # auxiliary Information
+        self.auxs = set()  # TODO:auxiliary Information
 
         for children in walktree(self.rootgroup):
             if self.rootgroup.variables:
                 for nvar, var in self.rootgroup.variables.items():
                     if 'coordinates' in var.ncattrs():
-                        self.vars_coord.add( "/" + nvar)
+                        self.vars_coord.add("/" + nvar)
                     else:
                         self.vars_meta.add("/" + nvar)
                 for dim in self.rootgroup.dimensions:
@@ -39,7 +36,7 @@ class NC4Info:
                         if 'coordinates' in var.ncattrs():
                             self.vars_coord.add(child.path + "/" + varn)
                             vcoords = getattr(var, 'coordinates')
-                            if not "," in vcoords:
+                            if "," not in vcoords:
                                 self.coords.add(child.path + "/" + getattr(var, 'coordinates'))
                             else:
                                 nvc = vcoords.split(", ")
@@ -50,11 +47,12 @@ class NC4Info:
                     for dim in child.dimensions:
                         self.dims.add(child.path + "/" + dim)
 
-    def get_sci_vars(self):
-        return info4.vars_coord - info4.vars_meta - info4.dims - info4.coords
+    def get_science_variables(self):
+        return self.vars_coord - self.vars_meta - self.dims - self.coords
 
-    def get_meta_vars(self):
-        return info4.vars_meta - info4.dims - info4.coords
+    def get_metadata_variables(self):
+        return self.vars_meta - self.dims - self.coords
+
 
 # Main program start for testing with any input file
 #
@@ -67,7 +65,7 @@ if __name__ == "__main__":
     logger = logging.getLogger("SwotRepr")
     syslog = logging.StreamHandler()
     formatter = logging.Formatter(
-            "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s")
+        "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s")
     #       "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] [%(user)s] %(message)s")
     syslog.setFormatter(formatter)
     logger.addHandler(syslog)
@@ -76,14 +74,12 @@ if __name__ == "__main__":
 
     input_file = ARGS.file
 
-#----------------------------------------
+    # ----------------------------------------
     info4 = NC4Info(input_file)
     print(info4.rootgroup.data_model)
-    sciVars = info4.get_sci_vars()
+    sciVars = info4.get_science_variables()
     print("--------- science_variables ----------")
-    print(*(sorted(sciVars)), sep ="\n")
-    metaVars = info4.get_meta_vars()
+    print(*(sorted(sciVars)), sep="\n")
+    metaVars = info4.get_metadata_variables()
     print("--------- metadata ----------")
     print(*(sorted(metaVars)), sep="\n")
-
-
