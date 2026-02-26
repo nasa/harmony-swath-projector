67fa3f7d1d30498c0773355de2cf62ee50184698
Vu Tran <108003017+vutrannasa@users.noreply.github.com>
2026-02-26 07:04:17 -0800

    DAS-2219 Accidently added test for local development

--- tests/test_input_file_download.py
+++ tests/test_input_file_download.py
@@ -1,4 +1,3 @@
-import logging
 from unittest import TestCase
 
 from harmony_service_lib.message import Message
@@ -48,47 +47,3 @@ class TestInputFileDownload(TestCase):
             )
 
         self.assertEqual(str(context.exception), 'Input file does not exist')
-
-    def test_local_tempo_03tot_l2_v2_das_2219(self):
-        """Handle a harmony message that refers to a granule local file that
-        does not exist
-
-        """
-        reprojector = SwathProjectorAdapter(
-            Message(
-                {
-                    "sources": [
-                        {
-                            "collection": "C1270257471-EEDTEST",
-                            "shortName": "TEMPO_O3TOT_L2",
-                            "versionId": "V03",
-                            "coordinateVariables": [],
-                            "visualizations": [],
-                        }
-                    ],
-                    "format": {
-                        "crs": "+proj=longlat +datum=WGS84 +no_defs",
-                        "srs": {
-                            "proj4": "+proj=longlat +datum=WGS84 +no_defs",
-                            "wkt": "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AXIS[\"Latitude\",NORTH],AXIS[\"Longitude\",EAST],AUTHORITY[\"EPSG\",\"4326\"]]",
-                            "epsg": "EPSG:4326",
-                        },
-                        "interpolation": "near",
-                        "mime": "application/netcdf",
-                    },
-                    "subset": {"dimensions": []},
-                }
-            ),
-            config=config(False),
-        )
-
-        reprojector.logger.setLevel(logging.DEBUG)
-
-        working_filename = reproject(
-            reprojector.message,
-            'TEMPO_O3TOT_L2',
-            'https://harmony.uat.earthdata.nasa.gov/service-results/harmony-uat-eedtest-data/C1270257471-EEDTEST/TEMPO_O3TOT_L2_V03_20230802T151249Z_S001G01.nc',
-            '/Users/vtran11/Documents/workspace/granules/TEMPO_O3TOT_L2_V03_20230802T151249Z_S001G01.nc',
-            '/Users/vtran11/Documents/workspace/harmony-swath-projector/tests/output_files/TEMPO_O3TOT_L2',
-            reprojector.logger,
-        )
