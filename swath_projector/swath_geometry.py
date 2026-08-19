b12739997bc7604601d0a1bd28cec7a3737020f3
Matt Savoie <matthew.savoie@colorado.edu>
2026-08-18 18:41:11 -0600

    DAS-2522: Use numpy.atan2 for consistency.

--- swath_projector/swath_geometry.py
+++ swath_projector/swath_geometry.py
@@ -5,7 +5,6 @@ extents and resolution of data in the projected Coordinate Reference System
 """
 
 import functools
-import math
 from typing import List, Tuple
 
 import numpy as np
@@ -273,7 +272,7 @@ def clockwise_point_sort(origin: List[float], point: List[float]) -> Tuple[float
         normalised_vector = np.divide(vector, vector_length)
         dot_product = np.dot(normalised_vector, reference_vector)
         determinant = np.linalg.det([normalised_vector, reference_vector])
-        vector_angle = math.atan2(determinant, dot_product)
+        vector_angle = np.atan2(determinant, dot_product)
 
     return vector_angle, vector_length
 
